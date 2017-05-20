a576a47809ac9e706f217fc3d7929d999e06839c
Evgeniy <mailerboss00@mail.com>
2017-05-20 18:23:48 +0300

    specified object.assign functionality into the user's class

--- src/models/app/user.ts
+++ src/models/app/user.ts
@@ -4,11 +4,11 @@ export interface IUser {
 }
 
 export class User implements IUser{
+
     public name: string;
     public age: number;
     
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
+    constructor(data: Object = {}) {
+        Object.assign(this, data);
     }
 }
