5ac569fe2829a0bce93f070d1735e28fd81f64c3
Djingo <mailerboss00@mail.com>
2017-10-15 12:32:22 +0300

    update request-api module for tests

--- test/config/request.ts
+++ test/config/request.ts
@@ -1,31 +1,5 @@
 import * as supertest from 'supertest';
 import { config } from "../../src/config/config";
 
-/**
- * Allows to manage http-requests for integration tests within test/api/ directory.
- * Uses a supertest module inside.
- */
-class Api {
-
-    private readonly baseUrl = `http://${config.host}:${config.port}`;
-    private readonly request = supertest(this.baseUrl);
-
-    public get(route: string): supertest.Request {
-        return this.request.get(route);
-    }
-
-    public post(route: string): supertest.Request {
-        return this.request.get(route);
-    }
-
-    public put(route: string): supertest.Request {
-        return this.request.get(route);
-    }
-
-    public delete(route: string): supertest.Request {
-        return this.request.get(route);
-    }
-
-}
-
-export const api = new Api();
+const baseUrl = `http://${config.host}:${config.port}`;
+export const api = supertest(baseUrl);
